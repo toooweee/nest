af967d784a848efeac2160b50bb97fcd49f6a350
GM 52. Office <byprob03@gmail.com>
2025-05-07 16:48:54 +0500

    idk what

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { MovieModule } from './movie/movie.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import * as process from 'node:process';
+import Joi from 'joi';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath:
+        process.env.NODE_ENV === 'development'
+          ? '.env.development'
+          : '.env.production',
+      validationSchema: Joi.object({}),
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
